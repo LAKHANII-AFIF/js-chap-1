5f61a438e59e60a0ff8cba4bbc0bc8dfb68b7ca5
LAKHANII-AFIF <154405157+LAKHANII-AFIF@users.noreply.github.com>
2024-02-26 21:54:49 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+                    //    chapter number 1
+                    // task Number 1
+alert("please enter a valid value")
+                    // task Number 2
+alert("welcome to js land \n happy coding")
+                    // task Number 3
+alert("welcome to js land") 
+alert("happy coding")
+                    //    chapter number 2
+                    // task Number 1
+let username = 'afif lakhani'
+                    // task Number 2
+let myName = "afif lakhani"
+                    // task Number 3
+var message = "hello world"
+alert(message)
+                    // task Number 4
+let fullName = "Afif Lakhani"
+let age = "17 year old"
+let qualification = "certified mobile application development"
+
+alert(fullName)
+alert(age)
+alert(qualification)
+                    // task Number 5
+let pizza = "pizza\npizz\npiz\npi\np"
+alert(pizza)
+                    // task Number 6
+let email = "afiflakhani83@gmail.com"
+alert("my email adress is" + " " + email)
+                    // task Number 7
+let book = "A smarter way to learn JavaScript"
+alert("i am trying to learn from the book" + " " + book)
+                    // task Number 8
+document.write("yah! I can write HTML content through javascript")
+                    // task Number 9
+let design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
